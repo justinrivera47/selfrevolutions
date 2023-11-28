9c53c84a2f3b978888c3541e0a78f4ccc24c0018
justinrivera47 <justinrivera47@gmail.com>
2023-11-28 09:05:38 -0500

    Added the calendar section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,44 @@ const Home = () => {
   return (
     <>
     <Hero />
+    {/* <div className='mid-div-container'>
+      <div className='mid-div'>
+
+      </div>
+    </div> */}
     <AboutSection />
     <Expertise />
     <Course />
+    <div className="blog-post-section-container">
+      <div className="blog-post-section">
+        <div className="cal-right-corner">15</div>
+        <p className='blog-post-section-author'>By James Rue</p>
+        <h4 className="blog-post-section-header">ICA Coaching</h4>
+        <p className="blog-post-section-text">8 Lessons</p>
+        <button className="hero-button">Read More</button>
+      </div>
+      <div className="blog-post-section">
+      <div className="cal-right-corner">15</div>
+        <p className='blog-post-section-author'>By James Rue</p>
+        <h4 className="blog-post-section-header">ICA Coaching</h4>
+        <p className="blog-post-section-text">8 Lessons</p>
+        <button className="hero-button">Read More</button>
+      </div>
+      <div className="blog-post-section">
+      <div className="cal-right-corner">15</div>
+        <p className='blog-post-section-author'>By James Rue</p>
+        <h4 className="blog-post-section-header">ICA Coaching</h4>
+        <p className="blog-post-section-text">8 Lessons</p>
+        <button className="hero-button">Read More</button>
+      </div>
+      <div className="blog-post-section">
+      <div className="cal-right-corner">15</div>
+        <p className='blog-post-section-author'>By James Rue</p>
+        <h4 className="blog-post-section-header">ICA Coaching</h4>
+        <p className="blog-post-section-text">8 Lessons</p>
+        <button className="hero-button">Read More</button>
+      </div>
+    </div>
     </>
   )
 }
